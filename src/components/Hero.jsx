75187a89cbac56bbc41cec966414512e25ccb067
Aarush <112254386+Hadar01@users.noreply.github.com>
2025-02-14 01:02:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Aarush</span>
+            Hi, I'm <span className='text-[#ff0000]'>Aarush</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Software Engineer
